b405235f0592a081c23e45006ed040aa649f5880
cghlee <143438779+cghlee@users.noreply.github.com>
2023-08-29 16:03:16 +0100

    Add error checks to URL, update `README.md`

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # vacancy-checker
- 
+
+An automated solution to filter through new job vacancies relevant to me,
+without having to reparse all 50+ vacancies daily while trying to remember
+those I had or had not seen. Originally designed for the UK Government's
+Civil Service Jobs website.
+
+## Dependencies
+
+This Vacancy Checker requires the `Requests` and `BeautifulSoup4` modules to
+function, with relevant documentation linked below:
+
+[Requests module](https://pypi.org/project/requests/)
+[BeautifulSoup4 module](https://pypi.org/project/beautifulsoup4/)
+
+## How it works
+
+Run `vacancy_checker.py`, which will call the `update_civil_service` function
+within `updater.py`.
+
+If a locally-stored `vacancies.json` file doesn't exist, it will be
+automatically generated using current job listings.
+
+If `vacancies.json` *does* exist locally, job vacancies will be updated and
+compared against those active at the last time the Vacancy Checker was run.
+New vacancies, if present, will be listed alongside a prompt for them to all
+be opened within your default browser.
--- updater.py
+++ updater.py
@@ -9,17 +9,19 @@ import requests, bs4, json
 def update_civil_service():
     # Pulls a Civil Service jobs URL and converts into a BS4 object
     URL_CIVIL_SERVICE = 'https://www.civilservicejobs.service.gov.uk/csr/'\
-                        'index.cgi?SID=Y29udGV4dGlkPTQ3NzA3OTUzJnBhZ2VhY3Rpb249c2'\
-                        'VhcmNoY29udGV4dCZvd25lcnR5cGU9ZmFpciZwYWdlY2xhc3M9U2Vhcm'\
-                        'NoJm93bmVyPTUwNzAwMDAmcGFnZT0xJnJlcXNpZz0xNjkzMTI5NTMxLT'\
-                        'llN2Q3M2Y0ZTA1MjM4YWM5YTEyNTk0ZTJjMDI5MzBhZjAyMmYwOTQ%3D'\
-                        '&sort=sallow&reqsig=1693129532-a9c8d0ccceec5415a00f02f22'\
-                        'ba66c02207980e2'
+                        'index.cgi?SID=cGFnZWFjdGlvbj1zZWFyY2hjb250ZXh0Jn'\
+                        'BhZ2VjbGFzcz1TZWFyY2gmcGFnZT0xJm93bmVyPTUwNzAwMD'\
+                        'Amb3duZXJ0eXBlPWZhaXImY29udGV4dGlkPTQ3ODYzMTIyJn'\
+                        'JlcXNpZz0xNjkzMzAyMjYzLWQ3N2FkMjU5Zjc0YTBiN2Q5MT'\
+                        'g1NDBiZjBkMzliMTM4MzM4OGFjNjM%3D&sort=sallow&req'\
+                        'sig=1693302263-d77ad259f74a0b7d918540bf0d39b1383388ac63'
     try:
         initial_page = requests.get(URL_CIVIL_SERVICE)
         initial_page.raise_for_status()
     except requests.exceptions.HTTPError as err:
-        return 'Error: %s' % str(err)
+        print('Error: %s' % str(err))
+        quit()
+
     initial_soup = bs4.BeautifulSoup(initial_page.text, 'html.parser')
     print('Updating Civil Service job vacancies')
 
@@ -27,7 +29,12 @@ def update_civil_service():
     results_element = initial_soup.find('div', 'csr-page-title').getText()
     total_results = results_element.split()[0]
     # Calculate expected number of pages of results (25 per page)
-    expected_pages = int(total_results) // 25
+    # If URL is invalid, raised ValueError will terminate the program
+    try:
+        expected_pages = int(total_results) // 25
+    except ValueError:
+        print('Invalid or expired URL\nPlease update the URL')
+        quit()
 
     # Grab HTML elements for the paging menu
     paging_element = initial_soup.select('div[class="search-results-paging-menu"]')
@@ -51,7 +58,8 @@ def update_civil_service():
             response_page = requests.get(page)
             response_page.raise_for_status()
         except requests.exceptions.HTTPError as err:
-            return 'Error: %s' % str(err)
+            print('Error: %s' % str(err))
+            quit()
         response_soup = bs4.BeautifulSoup(response_page.text, 'html.parser')
         page_results = response_soup.find_all('li', 'search-results-job-box')
         # Create nested dictionaries containing details about each job vacancy
